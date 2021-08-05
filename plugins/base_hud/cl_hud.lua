b47bdeed90f56083e6e345257065c45b35c53369
urnotnick <66189083+urnotnick@users.noreply.github.com>
2021-08-05 00:19:10 -0500

    SEXY AS FUCK HUD HOLY FUCK :100: :fire:

--- plugins/base_hud/cl_hud.lua
+++ plugins/base_hud/cl_hud.lua
@@ -13,10 +13,20 @@ hook.Add("HUDShouldDraw", "hudPlugin_hideDefault", function(elem)
 	end
 end)
 
-surface.CreateFont("hud18", {
+surface.CreateFont("hud24", {
 	font = "Arial",
 	shadow = false,
 	size = 24,
+	weight = 6000,
+	antialias = true,
+	extended = true,
+	italic = true
+})
+
+surface.CreateFont("hud18", {
+	font = "Arial",
+	shadow = false,
+	size = 18,
 	weight = 6000,
 	antialias = true,
 	extended = true,
@@ -35,7 +45,8 @@ local function drawBar(label,val,col,pos)
 		{x=pos.x+2,y=pos.y+2},
 		{x=pos.x+value+2,y=pos.y+2},
 		{x=pos.x+value+27,y=pos.y+27},
-		{x=pos.x+2,y=pos.y+27}
+		{x=pos.x+2,y=pos.y+27},
+		{x=pos.x,y=pos.y+25}
 	}
 	local r,g,b = col:Unpack()
 	surface.SetDrawColor(r/3, g/3, b/3,255)
@@ -45,8 +56,10 @@ local function drawBar(label,val,col,pos)
 	surface.SetMaterial(Material("vgui/gradient-l"))
 	surface.SetDrawColor(r/3, g/3, b/3,120)
 	surface.DrawTexturedRect(pos.x,pos.y,value,25)
-	draw.DrawText(tostring(val).." "..label, "hud18", pos.x+5, pos.y+2, color_black, TEXT_ALIGN_LEFT)
-	draw.DrawText(tostring(val).." "..label, "hud18", pos.x+3, pos.y+1, color_white, TEXT_ALIGN_LEFT)
+	draw.DrawText(tostring(val), "hud24", pos.x+5, pos.y+2, Color(r/3, g/3, b/3,255), TEXT_ALIGN_LEFT)
+	draw.DrawText(tostring(val), "hud24", pos.x+3, pos.y+1, color_white, TEXT_ALIGN_LEFT)
+	draw.SimpleText(label, "hud18", pos.x+2, pos.y, Color(r/3, g/3, b/3,255), TEXT_ALIGN_LEFT, TEXT_ALIGN_BOTTOM)
+	draw.SimpleText(label, "hud18", pos.x, pos.y-1, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_BOTTOM)
 	draw.NoTexture()
 end
 
@@ -54,9 +67,11 @@ local ply = LocalPlayer()
 local r,g,b = color_white:Unpack()
 
 hook.Add("HUDPaint", "hudPlugin_draw", function()
-	if not IsValid(ply) then return end
+	if not IsValid(ply) then 
+		ply = LocalPlayer()
+		return 
+	end
 	if not ply:Alive() then return end
-	drawBar("HP",ply:Health(),Color(255,0,0),{x=25,y=ScrH()-50})
-	if ply:Armor() <= 0 then return end
-	drawBar("Armor",ply:Armor(),Color(50,173,230),{x=25,y=ScrH()-80})
+	drawBar("Health",ply:Health(),Color(255,0,0),{x=25,y=ScrH()-50})
+	if ply:Armor() > 0 then drawBar("Armor",ply:Armor(),Color(50,173,230),{x=25,y=ScrH()-100}) end
 end)

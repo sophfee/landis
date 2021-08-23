73fe5a50597e317d4173a8c265dfea39561977ac
urnotnick <66189083+urnotnick@users.noreply.github.com>
2021-08-23 02:27:53 -0500

    Update init.lua

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -21,6 +21,15 @@ resource.AddSingleFile("materials/badges/smile.png")
 AddCSLuaFile("shared.lua")
 include("shared.lua")
 
+-- dev Branch stats & ver data
+local root = GM.FolderName
+local b = file.Open( "gamemodes/landis/.git/FETCH_HEAD", "r", "MOD" ):ReadLine()
+local branchID = "hi"
+local spl = string.Split(b, " ")
+spl[3] = string.gsub(spl[3], "of https://github.com/urnotnick/landys", "")
+spl[3] = string.Trim(spl[3])
+SetGlobalString(1, string.sub(spl[1],1,7))
+SetGlobalString(2, spl[3])
 
 hook.Add("PlayerSay", "DiscordWebhook", function(ply,text)
     http.Post("localhost:8080", {})

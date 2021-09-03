7d7b068a9b1b161398515f3e183547e898f7e3c9
urnotnick <66189083+urnotnick@users.noreply.github.com>
2021-09-02 23:43:23 -0500

    Update cl_spawnmenu.lua

--- core/cl_spawnmenu.lua
+++ core/cl_spawnmenu.lua
@@ -1,3 +1,7 @@
 hook.Add( "SpawnMenuOpen", "SpawnMenuWhitelist", function()
 	return LocalPlayer():IsAdmin()
 end )
+
+hook.Add( "AddToolMenuTabs", "myHookClass", function()
+	spawnmenu.AddToolTab( "Tab name!", "#Unique_Name", "icon16/wrench.png" )
+end )

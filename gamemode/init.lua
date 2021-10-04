e7516ec202a30ee291c60ac815b835854d41f0bf
Nick <66189083+urnotnick@users.noreply.github.com>
2021-10-03 23:54:57 -0500

    Update init.lua

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,27 +1,13 @@
-MsgC(Color(10,132,255),[[
-//////////////////////////////////
-            _                    
-           | |                   
-   __ _    | |__   __ _ ___  ___ 
-  / _` |   | '_ \ / _` / __|/ _ \
- | (_| |   | |_) | (_| \__ \  __/
-  \__, |   |_.__/ \__,_|___/\___|
-   __/ |_____                    
-  |___/______|                  
-
-//////////////////////////////////
-
-  g_base is a framework for gmod
-
-//////////////////////////////////
-]])
+MsgC(Color(10,132,255),"[landis] Initializing core elements...")
 
 resource.AddSingleFile("materials/badges/smile.png")
 
 AddCSLuaFile("shared.lua")
 include("shared.lua")
 
--- dev Branch stats & ver data
+INCLUDE_VERSION_DATA = false -- Keep off unless you have the git cloned!
+
+if not INCLUDE_VERSION_DATA then return end
 local root = GM.FolderName
 local b = file.Open( "gamemodes/landis/.git/FETCH_HEAD", "r", "MOD" ):ReadLine()
 local branchID = "hi"
@@ -30,7 +16,3 @@ spl[3] = string.gsub(spl[3], "of https://github.com/urnotnick/landys", "")
 spl[3] = string.Trim(spl[3])
 SetGlobalString(1, string.sub(spl[1],1,7))
 SetGlobalString(2, spl[3])
-
-hook.Add("PlayerSay", "DiscordWebhook", function(ply,text)
-    http.Post("localhost:8080", {})
-end)

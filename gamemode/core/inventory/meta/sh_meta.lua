5d1c4ca57ed754051ee924810eb51fd9b0b34419
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-07 13:07:43 -0600

    Update sh_meta.lua

--- gamemode/core/inventory/meta/sh_meta.lua
+++ gamemode/core/inventory/meta/sh_meta.lua
@@ -8,18 +8,23 @@ PLAYER.Inventory = {}
 --- [INTERNAL] Setup player inventory.
 -- [INTERNAL] Sets up inventory.
 function PLAYER:SetupInventory()
+	self.Inventory = {}
+end
 
+function PLAYER:Weight()
+	local weight = 0
+	for v,k in ipairs(self.Inventory) do
+		local item = landis.items.data[k.UniqueID]
+		weight = weight + item.weight
+	end
+	return weight
 end
 
 --- Can pickup item.
 -- monkey nuts.
 -- @param item Item that the user wants to pickup.
 function PLAYER:CanPickupItem( item )
-end
-
-function PLAYER:GetOpenSlot()
-	for v,k in ipairs( self.Inventory ) do
-		if not k then return v end
-	end
-	return false -- no slots available
+	local item = landis.items.data[item] or error()
+	local weight = self:Weight()
+	return !(weight + item.weight > 20)
 end

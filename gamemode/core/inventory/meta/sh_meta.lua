04cb6ba76570a67d6c002a8d4fa9bde6d7353064
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-07 15:26:00 -0600

    Update sh_meta.lua

--- gamemode/core/inventory/meta/sh_meta.lua
+++ gamemode/core/inventory/meta/sh_meta.lua
@@ -3,28 +3,3 @@
 
 local PLAYER = FindMetaTable( "Player" )
 
-PLAYER.Inventory = {}
-
---- [INTERNAL] Setup player inventory.
--- [INTERNAL] Sets up inventory.
-function PLAYER:SetupInventory()
-	self.Inventory = {}
-end
-
-function PLAYER:Weight()
-	local weight = 0
-	for v,k in ipairs(self.Inventory) do
-		local item = landis.items.data[k.UniqueID]
-		weight = weight + item.weight
-	end
-	return weight
-end
-
---- Can pickup item.
--- monkey nuts.
--- @param item Item that the user wants to pickup.
-function PLAYER:CanPickupItem( item )
-	local item = landis.items.data[item] or error()
-	local weight = self:Weight()
-	return !(weight + item.weight > 20)
-end

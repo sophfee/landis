1b333ee6bcb668fab0168c2dbde30ce3ec3a399f
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-07 15:05:25 -0600

    Update sv_player.lua

--- gamemode/core/meta/sv_player.lua
+++ gamemode/core/meta/sv_player.lua
@@ -1,1 +1,6 @@
+local PLAYER = FindMetaTable("Player")
 
+function PLAYER:SetXP( num )
+  sql.Query("UPDATE landis_user SET xp = " .. num .. " WHERE steamid = " .. sql.SQLStr( self:SteamID64() ) )
+  self:SetNWInt( "XP", num )
+end

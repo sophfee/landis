44e8d5cad9188acb8aa2264db8d9eb539952b072
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-07 15:08:21 -0600

    Update cl_player.lua

--- gamemode/core/meta/cl_player.lua
+++ gamemode/core/meta/cl_player.lua
@@ -1,1 +1,11 @@
-
+local PLAYER = FindMetaTable("Player")
+function meta:Notify(message,duration)
+	local panel = vgui.Create("landisNotify")
+	panel:SetDuration(duration or 5)
+	panel:SetMessage(message)
+end
+net.Receive("landisNotify", function()
+	local message = net.ReadString()
+	local duration = net.ReadInt(32)
+  LocalPlayer():Notify(message,duration)
+end)

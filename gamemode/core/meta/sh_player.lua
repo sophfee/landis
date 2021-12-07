2d932efb056f9d0f4842d801d6d78960cc38aaa2
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-07 15:04:43 -0600

    Update sh_player.lua

--- gamemode/core/meta/sh_player.lua
+++ gamemode/core/meta/sh_player.lua
@@ -1,1 +1,37 @@
-return -- cleaning files and moving them here
+local PLAYER = FindMetaTable("Player")
+
+function PLAYER:SetupDataTables()
+	self:NetworkVar("Bool", 0, "IsTyping")
+	self:NetworkVar("Int",1,"XP")
+	self:NetworkVar("Bool",2,"InNoclip")
+end
+
+function PLAYER:InNoclip()
+	return self:GetNWBool("InNoclip",false)
+end
+
+function PLAYER:GetRankName()
+  
+	if self:IsSuperAdmin() then
+		return "Community Manager"
+	end
+	if self:IsLeadAdmin() then
+		return "Lead Admin"
+	end
+	if self:IsAdmin() then
+		return "Admin"
+	end
+	return "User"
+end
+function PLAYER:GetPermissionLevel()
+	if self:IsSuperAdmin() then
+		return PERMISSION_LEVEL_SUPERADMIN
+	end
+	if self:IsLeadAdmin() then
+		return PERMISSION_LEVEL_LEAD_ADMIN
+	end
+	if self:IsAdmin() then
+		return PERMISSION_LEVEL_ADMIN
+	end
+	return PERMISSION_LEVEL_USER
+end

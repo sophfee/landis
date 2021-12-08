8d2b80e19aab72d4a57769e4255d1b4f1e019c7e
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-08 11:09:12 -0600

    Update cl_net.lua

--- gamemode/core/net/cl_net.lua
+++ gamemode/core/net/cl_net.lua
@@ -3,3 +3,11 @@ net.Receive("landisNotify", function()
 	local duration = net.ReadInt(32)
   LocalPlayer():Notify(message,duration)
 end)
+net.Receive("landisPickupItem",function()
+    local ply = net.ReadEntity()
+    local itm = net.ReadString()
+
+    local t = table.Copy(landis.items.data[itm])
+    
+    LocalPlayer().Inventory = table.ForceInsert(ply.Inventory,t)
+end)

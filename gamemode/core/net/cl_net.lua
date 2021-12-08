c60bdd363cceb8282d416fec13863cb33e974350
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-08 11:15:46 -0600

    Update cl_net.lua

--- gamemode/core/net/cl_net.lua
+++ gamemode/core/net/cl_net.lua
@@ -3,6 +3,7 @@ net.Receive("landisNotify", function()
 	local duration = net.ReadInt(32)
   LocalPlayer():Notify(message,duration)
 end)
+
 net.Receive("landisPickupItem",function()
     local ply = net.ReadEntity()
     local itm = net.ReadString()
@@ -10,4 +11,19 @@ net.Receive("landisPickupItem",function()
     local t = table.Copy(landis.items.data[itm])
     
     LocalPlayer().Inventory = table.ForceInsert(ply.Inventory,t)
+end)
+
+landis.VendorPanel = nil
+
+net.Receive("landisVendorOpen", function()
+	local ent = net.ReadEntity()
+	local class = net.ReadString()
+	local ven = net.ReadTable()
+
+	if not ent or not class then return end
+
+	if ven.Panel then
+		landis.VendorPanel = landis.VendorPanel or vgui.Create( ven.Panel ) 
+	end
+
 end)

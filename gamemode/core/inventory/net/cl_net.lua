f19466245ad92d0710ab9c79a36e2409d343d7c6
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-08 11:09:27 -0600

    Delete gamemode/core/inventory directory

--- gamemode/core/inventory/net/cl_net.lua
+++
@@ -1,8 +0,0 @@
-net.Receive("landisPickupItem",function()
-    local ply = net.ReadEntity()
-    local itm = net.ReadString()
-
-    local t = table.Copy(landis.items.data[itm])
-    
-    LocalPlayer().Inventory = table.ForceInsert(ply.Inventory,t)
-end)

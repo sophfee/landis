12aedfac7fa43e9b1fb9a68fd8d8fdf3ff575b2a
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-08 11:12:04 -0600

    Update sv_hooks.lua

--- gamemode/core/hooks/sv_hooks.lua
+++ gamemode/core/hooks/sv_hooks.lua
@@ -117,4 +117,34 @@ hook.Add("PlayerInitialSpawn", "landisStartMenu", function(ply,transition)
 	net.Send(ply)
 end)
 
+hook.Add("CheckPassword", "CheckForBan", function(uSteamID64)
+	local userData = sql.Query("SELECT * FROM landis_bans WHERE steamid = " .. sql.SQLStr(uSteamID64) .. ";")
+	if not userData then return true end
+	for _,ban in ipairs(userData) do
+		if os.time() < tonumber(ban.end_date) then 
+			return false, "uh oh! looks like you're currently banned!\n\nBanned by: " .. ban.moderator .. "\nReason: "..ban.reason.."\nUnban Date: "..os.date("%A, %B %d - %x %X", ban.end_date)  
+		end
+	end
+	return true
+end)
 
+hook.Add("PlayerInitialSpawn", "setup_data", function(ply)
+	ply:SetupData()
+end)
+
+
+hook.Add("PlayerDisconnected", "save_data", function(ply)
+	ply:SaveAllData()
+end)
+
+hook.Add("PlayerGiveSWEP","noThumpThump", function(ply,class,swep)
+	if class == "ls_thumpthump" then
+		return ply:SteamID() == "STEAM_0:1:92733650"
+	end
+end)
+
+hook.Add("PlayerSpawnSWEP","noThumpThump", function(ply,class,swep)
+	if class == "ls_thumpthump" then
+		return ply:SteamID() == "STEAM_0:1:92733650"
+	end
+end)

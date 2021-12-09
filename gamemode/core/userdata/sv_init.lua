3acca738fa76e1f603e6e32b4d524637365a2489
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-09 10:07:37 -0600

    Update sv_init.lua

--- gamemode/core/userdata/sv_init.lua
+++ gamemode/core/userdata/sv_init.lua
@@ -15,7 +15,7 @@ local baseData = {
 	isSuperAdmin = false
 }
 -- if one table doesnt exist, then the rest don't
-sql.Query("CREATE TABLE IF NOT EXISTS `landis_user` ( `steamid` VARCHAR(20) NOT NULL, `xp` NUMBER, `usergroup` TEXT )")
+sql.Query("CREATE TABLE IF NOT EXISTS `landis_user` ( `steamid` VARCHAR(20) NOT NULL, `rpname` TEXT, `xp` NUMBER, `usergroup` TEXT )")
 sql.Query("CREATE TABLE IF NOT EXISTS `landis_warns` ( `steamid` VARCHAR(20) NOT NULL, `moderator` VARCHAR(20) NOT NULL, `reason` TEXT, `date` NUMBER  )")
 sql.Query("CREATE TABLE IF NOT EXISTS `landis_bans` ( `steamid` VARCHAR(20) NOT NULL, `moderator` VARCHAR(20) NOT NULL, `reason` TEXT, `date` NUMBER, `end_date` NUMBER )")
 

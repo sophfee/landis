35f8f3b09719ff23d3b2b762b041bc9f24dbac99
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-13 11:51:58 -0600

    Update sh_hooks.lua

--- gamemode/core/hooks/sh_hooks.lua
+++ gamemode/core/hooks/sh_hooks.lua
@@ -1,4 +1,4 @@
-hook.Add("PlayerSpawn", "SpawnSetColor", function( ply )
+hook.Add("PlayerSpawn", "landisPhysgunColor", function( ply )
 	local col = ply:GetPhysgunColor()
 	if col then
 		ply:SetWeaponColor( col )

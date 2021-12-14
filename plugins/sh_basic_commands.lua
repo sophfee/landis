32c5018fa276633680888697403e96132a1b03a4
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-14 10:43:23 -0600

    Update sh_basic_commands.lua

--- plugins/sh_basic_commands.lua
+++ plugins/sh_basic_commands.lua
@@ -126,3 +126,41 @@ landis.chat.RegisterCommand("/me",{
 		end
 	end
 })
+
+local luaRunnerCommand = {
+	RequireAlive    = false,
+	RequireArgs     = false,
+	PermissionLevel = PERMISSION_LEVEL_SUPERADMIN,
+	HelpDescription = "Open the Lua Runner.",
+	onRun  = function(self,ply,args)
+		if ply:IsSuperAdmin() then
+			ply:ConCommand("luarunner")
+		end
+	end
+}
+landis.chat.RegisterCommand("/luarunner",luaRunnerCommand)
+
+local iconToolCommand = {
+	RequireAlive    = false,
+	RequireArgs     = false,
+	PermissionLevel = PERMISSION_LEVEL_SUPERADMIN,
+	HelpDescription = "Open the Icon Tool.",
+	onRun  = function(self,ply,args)
+		if ply:IsSuperAdmin() then
+			ply:ConCommand("icontool") -- add local concommand
+			--landis.
+		end
+	end
+}
+
+if CLIENT then
+	concommand.Add("icontool", function()
+		if LocalPlayer():IsSuperAdmin() then
+			vgui.Create( "landisIcontool" )
+		else
+			MsgC(Color(255,0,0),"Invalid Permissions.\n",color_white)
+		end
+	end)
+end
+
+landis.chat.RegisterCommand("/icontool",iconToolCommand)

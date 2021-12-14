cdbb92476b78132ce05173d21e18ce4e30d0cf7d
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-14 10:42:21 -0600

    Update sh_basic_commands.lua

--- plugins/sh_basic_commands.lua
+++ plugins/sh_basic_commands.lua
@@ -111,3 +111,18 @@ local dropmoneyCommand = {
 }
 
 landis.chat.RegisterCommand("/dropmoney",dropmoneyCommand)
+
+-- landis.chat.commands
+landis.chat.RegisterCommand("/me",{
+	RequireAlive    = true,
+	RequireArgs     = false,
+	PermissionLevel = PERMISSION_LEVEL_USER,
+	HelpDescription = "Make yourself perform an action",
+	onRun  = function(self,ply,args)
+		for v,k in ipairs(player.GetHumans()) do
+			if hook.Run("PlayerCanSeePlayersChat", "ME_MESSAGE", false, k, ply) then
+				k:AddChatText(Color(255,200,40),ply:GetRPName()," ",table.concat(args," "))
+			end
+		end
+	end
+})

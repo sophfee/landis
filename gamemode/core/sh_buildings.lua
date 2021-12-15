0cdd8036f9144982f5ab38ba1fe4a0f9f8b25f3d
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-15 08:59:22 -0600

    Update and rename sh_doors.lua to sh_buildings.lua

--- gamemode/core/sh_buildings.lua
+++ gamemode/core/sh_buildings.lua
@@ -2,5 +2,8 @@ landis.Buildings = landis.Buildings or {}
 landis.Buildings.Data = landis.Buildings.Data or {}
 
 function landis.Buildings.Register(UniqueID,self)
-    
+    if SERVER then
+        landis.ConsoleMessage("Registered Building ID: " .. UniqueID)
+    end
+    landis.Buildings.Data[UniqueID] = self
 end

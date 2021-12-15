2f4189184b99ccee74e23454f34c5e96f9826cd7
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-15 12:19:35 -0600

    Update cl_scenes.lua

--- gamemode/core/cl_scenes.lua
+++ gamemode/core/cl_scenes.lua
@@ -9,13 +9,15 @@ landis.Scene.fromPos   = nil
 landis.Scene.fromAng   = nil
 landis.Scene.toPos     = nil
 landis.Scene.toAng     = nil
+landis.Scene.Fading    = false
 landis.Scene.Tween     = nil
 
 function landis.PlayScene(self)
   self.IsPlaying = true
   -- faster inheritence?? idk test the performance of this.
   landis.Scene = table.Inherit(landis.Scene,self)
-  landis.Playhead = 0
+  landis.Scene.Fading = false
+  landis.Scene.Playhead = 0
   landis.Scene.Tween = tween.new(self.Duration, landis.Scene, {"Playhead"=self.Duration}, "outInQuad")
   LocalPlayer():ScreenFade(SCREENFADE.IN,Color(0,0,0,255),0.25,0)
 end
@@ -27,5 +29,13 @@ hook.Add("CalcView","landisPlayScene",function()
     local view = {}
     view.origin = LerpVector(scene.Playhead/scene.Duration,scene.fromPos,scene.toPos)
     view.angles = LerpAngle(scene.Playhead/scene.Duration,scene.fromAng,scene.toAng)
+    if scene.Playhead > scene.Duration - .25 and not scene.Fading then
+      landis.Scene.Fading = true
+      LocalPlayer():ScreenFade(SCREENFADE.IN,Color(0,0,0,255),.25,0)
+      timer.Simple(.25, function()
+          landis.Scene.IsPlaying = false
+          LocalPlayer():ScreenFade(SCREENFADE.OUT,Color(0,0,0,255),.75,0)
+      end)
+    end
     return view
 end)

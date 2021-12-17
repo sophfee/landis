b49fad3a0a41133f988ea97a629b7e86040e87c9
Nick <66189083+urnotnick@users.noreply.github.com>
2021-12-17 12:56:31 -0600

    Create shared.lua

---
+++ entities/entities/landis_loot_container/shared.lua
@@ -0,0 +1,8 @@
+AddCSLuaFile()
+
+ENT.PrintName = "Loot Container"
+ENT.Base      = "base_gmodentity"
+ENT.Type      = "anim"
+ENT.Category  = "landis"
+ENT.name      = "Lootable Container"
+ENT.desc      = "Sometimes some materials and gear can appear here."

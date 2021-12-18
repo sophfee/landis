2af370ea6e70a91039ed001b4911d9d67c54d272
Nick <iamquicknick@gmail.com>
2021-12-17 23:12:27 -0600

    Finished anim system (for now)

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,4 +1,5 @@
 MsgC(Color(10,132,255),"[landis] Initializing core elements...\n")
+RunConsoleCommand("mp_falldamage","1")
 -- Just a little badge file, nothing special
 resource.AddSingleFile("materials/badges/smile.png") -- any smilers?
 AddCSLuaFile("shared.lua")
